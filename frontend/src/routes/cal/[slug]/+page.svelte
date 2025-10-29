5cd1c1603f06bb431650a0278ad0892358c11784
Stephen Gruzin <stephengrzn@gmail.com>
2025-10-28 19:54:46 -0700

    Added the fullscreen functionality

--- frontend/src/routes/cal/[slug]/+page.svelte
+++ frontend/src/routes/cal/[slug]/+page.svelte
@@ -1,13 +1,30 @@
 <script lang="ts">
     import Calendar from '@/Calendar.svelte';
     import * as Popover from "$lib/components/ui/popover/index.js";
-    import { CircleQuestionMark } from '@lucide/svelte';
+    import { CircleQuestionMark, Fullscreen } from '@lucide/svelte';
     import { Label } from '@/components/ui/label/index.js';
     import { Switch } from '@/components/ui/switch/index.js';
+    import Button from '@/components/ui/button/button.svelte';
 
     let { data } = $props();
 
     let invisibleTooltip = $state(false);
+
+    function fullscreen() {
+        const elem = document.getElementById("cal-root");
+        if (!elem) return
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+            //@ts-ignore
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            //@ts-ignore
+            elem.webkitRequestFullscreen();
+            //@ts-ignore
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            //@ts-ignore
+            elem.msRequestFullscreen();
+        }
+    }
 </script>
 
 <svelte:head>
@@ -15,7 +32,7 @@
     <link rel="shortcut icon" href={data.logoLink} type="image/x-icon">
 </svelte:head>
 
-<div class="dark min-h-screen w-full p-6 bg-background relative">
+<div id="cal-root" class="dark min-h-screen w-full p-6 bg-background relative">
     <Calendar events={data.events} />
     <Popover.Root>
         <Popover.Trigger class="absolute bottom-1 right-1 z-50 {invisibleTooltip ? "text-background bg-background" : "text-muted-foreground bg-foreground"} rounded p-2" style={invisibleTooltip ? "border:none;" : "border: 1px solid #333333"}>
@@ -26,6 +43,7 @@
                 <Label for="useinvis" class="dark text-sm">Hide tooltip (you can still click it)</Label>
                 <Switch class="dark" id="useinvis" bind:checked={invisibleTooltip} />
             </div>
+            <Button onclick={fullscreen} title="Fullscreen"><Fullscreen /></Button>
         </Popover.Content>
     </Popover.Root>
 </div>
