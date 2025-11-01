c78710b300bdec67bdd0d7d4be3272f347dae9be
Stephen Gruzin <stephengrzn@gmail.com>
2025-10-31 23:36:26 -0700

    Added singular and plural checker for the rooms and resources

--- frontend/src/lib/EventResources.svelte
+++ frontend/src/lib/EventResources.svelte
@@ -23,7 +23,7 @@
     <div class="flex items-start gap-2 text-sm text-gray-400">
         <Warehouse class="h-4 w-4 mt-0.5 flex-shrink-0" />
         <p>
-            <span class="text-gray-400">Room:</span>
+            <span class="text-gray-400">Room{#if rooms.length > 1}s{/if}:</span>
             <span class="text-gray-300">
                 {#each rooms as room, index}
                     {#if room.path_name}{room.path_name}\\{/if} {room.name}{#if index+1 < rooms.length}, {/if}
@@ -37,7 +37,7 @@
     <div class="flex items-start gap-2 text-sm text-gray-400">
         <Package class="h-4 w-4 mt-0.5 flex-shrink-0" />
         <p>
-            <span class="text-gray-400">Resources:</span>
+            <span class="text-gray-400">Resource{#if res.length > 1}s{/if}:</span>
             <span class="text-gray-300">
                 {#each res as resource, index}
                     {resource.name}{#if resource.path_name && showResourcePathname}<span class="pl-1">({resource.path_name})</span>{/if}{#if index+1 < res.length}<span class="pr-1">, </span> {/if}
