2644a8b8f0bb7a9ae572fdc08a33c033596af2ae
Stephen Gruzin <stephengrzn@gmail.com>
2025-12-01 15:35:22 -0800

    Removed all the previous events that were fetched. This cleans up space and removes duplicates.

--- functions/GetAndStoreNextEvents.go
+++ functions/GetAndStoreNextEvents.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/Stephen10121/tailorcalbackend/planningcenter"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 )
@@ -25,6 +26,46 @@ func GetAndStoreNextThreeEvents(userId string, app *pocketbase.PocketBase) {
 	if err != nil {
 		app.Logger().Warn("Create the users collection to save the data fetched from the planning center api!")
 		return
+	}
+
+	currentEvents, err := app.FindAllRecords("events",
+		dbx.NewExp("owner = {:ownerid}", dbx.Params{"ownerid": userId}),
+	)
+
+	if err != nil {
+		app.Logger().Error(
+			"Failed to fetch the users current events!",
+			"user id", userId,
+			"error", err,
+		)
+		return
+	}
+
+	// delete all previous records to save space and remove duplicates.
+	err = app.RunInTransaction(func(txApp core.App) error {
+		for _, event := range currentEvents {
+			err = txApp.Delete(event)
+
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		app.Logger().Error(
+			"Failed to delete the users previous records!",
+			"user id", userId,
+			"error", err,
+		)
+		return
+	} else {
+		app.Logger().Info(
+			"Successfully deleted the users previous records!",
+			"user id", userId,
+		)
 	}
 
 	for i := 0; i < len(events); i++ {
